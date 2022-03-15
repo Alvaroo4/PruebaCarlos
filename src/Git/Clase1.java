25fe20f661491c80cfac31f37ee3965c994e8cad
Alvaroo4 <alvaroo1008@gmail.com>
2022-03-15 17:14:44 +0100

    Modificacion del proyecto Clase1.java

---
+++ src/Git/Clase1.java
@@ -0,0 +1,13 @@
+package Git;
+public class Clase1 {
+
+	public static void main(String[] args) {
+		// FIXME Auto-generated method stub
+		System.out.println("Hola mundo");
+		System.out.println("Adios mundo");
+		System.out.println("Hola de nuevo");
+		System.out.println("Adios de nuevo");
+
+	}
+
+}
